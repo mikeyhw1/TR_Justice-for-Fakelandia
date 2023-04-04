57d7d41c894d5a16546f71ceeaa4c8bc364bd56e
mikeyhw1 <70017382+mikeyhw1@users.noreply.github.com>
2023-04-04 17:28:34 +0100

    feat: added select and filter in Misdemeanour

--- client/src/pages/Misdemeanours/Datalist.tsx
+++ client/src/pages/Misdemeanours/Datalist.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useEffect, useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -7,16 +8,28 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { Misdemeanour, MisdemeanourKind } from "../../types/misdemeanours.types";
+import MenuItem from "@mui/material/MenuItem";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { InputLabel } from "@mui/material";
 
 interface DatalistProps {
-    // dataAmount: number;
     data: Misdemeanour[];
 }
 
 const Datalist: React.FC<DatalistProps> = ({ data }) => {
-    React.useEffect(() => {
-        console.log(data);
-    }, []);
+    const [filteredData, setFilteredData] = useState<Misdemeanour[]>();
+
+    const [selectedKey, setSelectedKey] = useState<MisdemeanourKind | "all">("all");
+
+    useEffect(() => {
+        selectedKey === "all"
+            ? setFilteredData(data)
+            : setFilteredData(data.filter((item) => item.misdemeanour === selectedKey));
+    }, [selectedKey]);
+
+    const handleChange = (event: SelectChangeEvent) => {
+        setSelectedKey(event.target.value as MisdemeanourKind | "all");
+    };
 
     const misdemeanourIcon = (input: MisdemeanourKind) => {
         switch (input) {
@@ -34,32 +47,54 @@ const Datalist: React.FC<DatalistProps> = ({ data }) => {
     };
 
     return (
-        <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Citizen ID</TableCell>
-                        <TableCell align="left">Date</TableCell>
-                        <TableCell align="left">Misdemeanour</TableCell>
-                        <TableCell align="right">Punishment Idea</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {data.map((row) => (
-                        <TableRow key={row.citizenId} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-                            <TableCell component="th" scope="row">
-                                {row.citizenId}
-                            </TableCell>
-                            <TableCell align="left">{row.date}</TableCell>
-                            <TableCell align="left">
-                                {row.misdemeanour} {misdemeanourIcon(row.misdemeanour)}
-                            </TableCell>
-                            <TableCell align="right">___</TableCell>
+        <div>
+            <InputLabel>Filter:</InputLabel>
+            <Select
+                className="select"
+                labelId="demo-simple-select-autowidth-label"
+                id="demo-simple-select-autowidth"
+                value={selectedKey}
+                onChange={handleChange}
+                autoWidth
+            >
+                <MenuItem value={"all"}>All</MenuItem>
+                <MenuItem value={"rudeness"}>Mild Public Rudeness</MenuItem>
+                <MenuItem value={"vegetables"}>Not Eating Your Vegetables</MenuItem>
+                <MenuItem value={"lift"}>Speaking in a Lift</MenuItem>
+                <MenuItem value={"united"}>Supporting Manchester United</MenuItem>
+            </Select>
+
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Citizen ID</TableCell>
+                            <TableCell align="left">Date</TableCell>
+                            <TableCell align="left">Misdemeanour</TableCell>
+                            <TableCell align="right">Punishment Idea</TableCell>
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                    </TableHead>
+                    <TableBody>
+                        {filteredData &&
+                            filteredData.map((row) => (
+                                <TableRow
+                                    key={row.citizenId}
+                                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                                >
+                                    <TableCell component="th" scope="row">
+                                        {row.citizenId}
+                                    </TableCell>
+                                    <TableCell align="left">{row.date}</TableCell>
+                                    <TableCell align="left">
+                                        {row.misdemeanour} {misdemeanourIcon(row.misdemeanour)}
+                                    </TableCell>
+                                    <TableCell align="right">___</TableCell>
+                                </TableRow>
+                            ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
     );
 };
 
